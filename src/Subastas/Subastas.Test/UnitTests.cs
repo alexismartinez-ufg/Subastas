abd3fe3a5f24024a6ab94c5bad637b557b98339a
Alexis Martínez <ia.alexismartinez@ufg.edu.sv>
2025-05-29 18:35:18 -0600

    :white_check_mark: agregar pruebas unitarias de login y creación de subastas

--- src/Subastas/Subastas.Test/UnitTests.cs
+++ src/Subastas/Subastas.Test/UnitTests.cs
@@ -1,21 +1,24 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Subastas.Domain;
 using Subastas.Interfaces;
+using Subastas.Interfaces.Services;
 using Subastas.TextFixture;
 using System.Collections.ObjectModel;
 
-namespace Subastas.Test.Auth
+namespace Subastas.Test
 {
-    public class _01_UserRegisterTest(TestFixture fixture) : IClassFixture<TestFixture>
+    public class UnitTests(TestFixture fixture) : IClassFixture<TestFixture>
     {
         private readonly IMenuService MenuService = fixture.ServiceProvider.GetService<IMenuService>()!;
         private readonly IPermisoService PermisoService = fixture.ServiceProvider.GetService<IPermisoService>()!;
         private readonly IRolService RolService = fixture.ServiceProvider.GetService<IRolService>()!;
         private readonly IUserService UserService = fixture.ServiceProvider.GetService<IUserService>()!;
         private readonly IEncryptionService EncrypManager = fixture.ServiceProvider.GetService<IEncryptionService>()!;
+        private readonly IProductoService ProductoService = fixture.ServiceProvider.GetService<IProductoService>()!;
+        private readonly ISubastaService SubastaService = fixture.ServiceProvider.GetService<ISubastaService>()!;
 
-        [Fact]
-        public async Task RegisterUser_WithValidData_ShouldCreateUser()
+        [Fact(DisplayName = "01 - Crear usuario")]
+        public async Task Test_01_RegisterUser_WithValidData_ShouldCreateUser()
         {
             var menu = await MenuService.CreateIfNotExistsAsync(new Menu
             {
@@ -64,6 +67,44 @@ namespace Subastas.Test.Auth
             });
 
             Assert.NotNull(Alexis);
+        }
+
+        [Fact(DisplayName = "02 - Login exitoso")]
+        public async Task Test_02_Login_WithValidCredentials_ShouldReturnUser()
+        {
+            string email = "ia.alexismartinez@ufg.edu.sv";
+            string password = "MiContraseñaEnEntornoDev";
+
+            var user = await UserService.GetUserAndRoleByLogin(email, password);
+
+            Assert.NotNull(user);
+        }
+
+        [Fact(DisplayName = "03 - Creación de Subasta")]
+        public async Task Test_03_CreateAuction_AsAdmin_ShouldSucceed()
+        {
+            var producto = new Producto
+            {
+                NombreProducto = "Producto de Unit Testing",
+                DescripcionProducto = "Producto utilizado en pruebas de unit testing para asociarlo a una subasta.",
+                ImagenProducto = "dirección de una imagen ficticia"
+            };
+
+            producto = await ProductoService.CreateAsync(producto);
+
+            var subasta = new Domain.Subasta
+            {
+                TituloSubasta = "Subasta en Unit Testing",
+                MontoInicial = 100.00m,
+                FechaSubasta = DateTime.Now.AddHours(1),
+                FechaSubastaFin = DateTime.Now.AddDays(1),
+                IdProducto = producto.IdProducto
+            };
+
+            subasta = await SubastaService.CreateAsync(subasta);
+
+            Assert.NotNull(subasta);
+            Assert.True(subasta.IdProducto > 0);
         }
     }
 }
