ff7f97a15d4cf6d681c65d1b7ed60f4a9c284a3f
Alexis Martínez <ia.alexismartinez@ufg.edu.sv>
2025-05-29 21:58:43 -0600

    :white_check_mark: refactorizar los unit test y agregar ell numero 10

--- src/Subastas/Subastas.Test/UnitTests.cs
+++ src/Subastas/Subastas.Test/UnitTests.cs
@@ -3,39 +3,36 @@ using Subastas.Domain;
 using Subastas.Interfaces;
 using Subastas.Interfaces.Services;
 using Subastas.TextFixture;
-using System.Collections.ObjectModel;
 
 namespace Subastas.Test
 {
     public class UnitTests(TestFixture fixture) : IClassFixture<TestFixture>
     {
-        private readonly IMenuService MenuService = fixture.ServiceProvider.GetService<IMenuService>()!;
-        private readonly IPermisoService PermisoService = fixture.ServiceProvider.GetService<IPermisoService>()!;
-        private readonly IRolService RolService = fixture.ServiceProvider.GetService<IRolService>()!;
-        private readonly IUserService UserService = fixture.ServiceProvider.GetService<IUserService>()!;
-        private readonly IEncryptionService EncrypManager = fixture.ServiceProvider.GetService<IEncryptionService>()!;
-        private readonly IProductoService ProductoService = fixture.ServiceProvider.GetService<IProductoService>()!;
-        private readonly ISubastaService SubastaService = fixture.ServiceProvider.GetService<ISubastaService>()!;
-        private readonly IPujaService PujaService = fixture.ServiceProvider.GetService<IPujaService>()!;
-        private readonly ICuentaService CuentaService = fixture.ServiceProvider.GetService<ICuentaService>()!;
+        private readonly IMenuService MenuService = fixture.ServiceProvider.GetRequiredService<IMenuService>();
+        private readonly IPermisoService PermisoService = fixture.ServiceProvider.GetRequiredService<IPermisoService>();
+        private readonly IRolService RolService = fixture.ServiceProvider.GetRequiredService<IRolService>();
+        private readonly IUserService UserService = fixture.ServiceProvider.GetRequiredService<IUserService>();
+        private readonly IEncryptionService EncrypManager = fixture.ServiceProvider.GetRequiredService<IEncryptionService>();
+        private readonly IProductoService ProductoService = fixture.ServiceProvider.GetRequiredService<IProductoService>();
+        private readonly ISubastaService SubastaService = fixture.ServiceProvider.GetRequiredService<ISubastaService>();
+        private readonly IPujaService PujaService = fixture.ServiceProvider.GetRequiredService<IPujaService>();
+        private readonly ICuentaService CuentaService = fixture.ServiceProvider.GetRequiredService<ICuentaService>();
 
         [Fact(DisplayName = "01 - Crear usuario")]
         public async Task RegisterUser_WithValidData_ShouldCreateUser()
         {
-            var alexis = await CreatUser();
-
-            Assert.NotNull(alexis);
+            var user = await CreateUser();
+            Assert.NotNull(user);
         }
 
         [Fact(DisplayName = "02 - Login exitoso")]
         public async Task Login_WithValidCredentials_ShouldReturnUser()
         {
-            await CreatUser();
+            await CreateUser();
 
-            string email = "ia.alexismartinez@ufg.edu.sv";
-            string password = "MiContraseñaEnEntornoDev";
-
-            var user = await UserService.GetUserAndRoleByLogin(email, password);
+            var user = await UserService.GetUserAndRoleByLogin(
+                "ia.alexismartinez@ufg.edu.sv",
+                "MiContraseñaEnEntornoDev");
 
             Assert.NotNull(user);
         }
@@ -44,7 +41,6 @@ namespace Subastas.Test
         public async Task CreateAuction_AsAdmin_ShouldSucceed()
         {
             var subasta = await CreateSubasta();
-
             Assert.NotNull(subasta);
             Assert.True(subasta.IdProducto > 0);
         }
@@ -53,67 +49,21 @@ namespace Subastas.Test
         public async Task EditProduct_ShouldUpdateProductDetails_WhenProductExists()
         {
             var producto = await CreateProducto();
-
-            if (producto == null)
-                Assert.Fail("El producto con nombre \"Producto de Unit Testing\" no se encontro.");
+            Assert.NotNull(producto);
 
             producto.DescripcionProducto = "Descripción modificada";
             producto.ImagenProducto = "Imagen modificada";
 
             var productoEditado = await ProductoService.UpdateAsync(producto);
 
-            Assert.NotNull(producto);
             Assert.Equal("Descripción modificada", productoEditado.DescripcionProducto);
             Assert.Equal("Imagen modificada", productoEditado.ImagenProducto);
         }
 
-        [Fact(DisplayName = "05 - Edición de Producto")]
+        [Fact(DisplayName = "05 - Pujar con monto válido")]
         public async Task PlaceBid_WithValidAmount_ShouldUpdateAuction()
         {
-            var usuario = await CreatUser();
-
-            if (usuario == null)
-                Assert.Fail("El usuario con correo \"ia.alexismartinez@ufg.edu.sv\" no se encontro.");
-
-            var usuarioWithCuentum = await UserService.GetUserWithCuentum(usuario.IdUsuario);
-
-            if (usuarioWithCuentum == null || usuarioWithCuentum.Cuentum == null)
-                Assert.Fail($"El usuario con id {usuario.IdUsuario} no se encontro o no tiene cuenta.");
-
-            var subasta = await CreateSubasta();
-
-            if (subasta == null)
-                Assert.Fail("La subasta con nombre \"Subasta en Unit Testing\" no se encontro.");
-
-            var puja = new Puja()
-            {
-                FechaPuja = DateTime.Now,
-                IdUsuario = usuario.IdUsuario,
-                IdSubasta = subasta.IdSubasta,
-                MontoPuja = 200.00m
-            };
-
-            if ((puja.MontoPuja - usuarioWithCuentum.Cuentum.Saldo) > 0)
-            {
-                Assert.Fail($"Saldo insuficiente del usuario {usuario.CorreoUsuario}");
-            }
-
-            var pujasSubasta = await PujaService.GetAllByPredicateAsync(x => x.IdSubasta == puja.IdSubasta);
-
-            if (pujasSubasta.Any() && puja.MontoPuja <= pujasSubasta.Max(p => p.MontoPuja))
-                Assert.Fail($"La puja actual: {pujasSubasta.Max(p => p.MontoPuja)} es mayor al monto enviado: {puja.MontoPuja}");
-
-            decimal saldo = usuarioWithCuentum.Cuentum.Saldo - puja.MontoPuja;
-
-            var cuenta = await CuentaService.GetByUserIdAsync(puja.IdUsuario);
-            cuenta.Saldo = saldo;
-            bool update = await CuentaService.UpdateCuenta(cuenta);
-
-            if (!update)
-                Assert.Fail($"No se actualizó el saldo del usuario {usuario.CorreoUsuario}");
-
-            var nuevaPuja = await PujaService.CreateAsync(puja);
-
+            var nuevaPuja = await CreatePuja();
             Assert.NotNull(nuevaPuja);
             Assert.Equal(200.00m, nuevaPuja.MontoPuja);
         }
@@ -122,12 +72,9 @@ namespace Subastas.Test
         public async Task CancelAuction_ByOwnerOrAdmin_ShouldSetStatusToCancelled()
         {
             var subasta = await CreateSubasta();
-
-            if (subasta == null)
-                Assert.Fail("La subasta con nombre \"Subasta en Unit Testing\" no se encontro.");
+            Assert.NotNull(subasta);
 
             var deleted = await SubastaService.DeleteById(subasta.IdSubasta);
-
             Assert.True(deleted);
         }
 
@@ -135,27 +82,85 @@ namespace Subastas.Test
         public async Task EndAuction_WhenExpirationReached_ShouldCloseAuction()
         {
             var subasta = await CreateSubasta();
-
-            if (subasta == null)
-                Assert.Fail("La subasta con nombre \"Subasta en Unit Testing\" no se encontro.");
+            Assert.NotNull(subasta);
 
             subasta.EstaActivo = false;
-
             await SubastaService.UpdateAsync(subasta);
 
-            Assert.NotNull(subasta);
             Assert.False(subasta.EstaActivo);
         }
 
-        // METODOS AUXILIARES
-        public async Task<Usuario> CreatUser()
+        [Fact(DisplayName = "08 - Saldo Insuficiente para pujar")]
+        public async Task PlaceBid_WithInsufficientBalance_ShouldFail()
         {
-            var menu = await MenuService.CreateIfNotExistsAsync(new Menu
-            {
-                EstaActivo = true,
-                NombreMenu = "Authentication",
-            });
+            var usuario = await CreateUser();
+            var cuenta = (await UserService.GetUserWithCuentum(usuario.IdUsuario)).Cuentum!;
+            var subasta = await CreateSubasta();
 
+            var puja = new Puja
+            {
+                FechaPuja = DateTime.Now,
+                IdUsuario = usuario.IdUsuario,
+                IdSubasta = subasta.IdSubasta,
+                MontoPuja = 20000.00m
+            };
+
+            Assert.True(puja.MontoPuja > cuenta.Saldo);
+        }
+
+        [Fact(DisplayName = "09 - Historial de participaciones en subastas")]
+        public async Task GetAuctionHistory_ByUser_ShouldReturnCorrectData()
+        {
+            var usuario = await CreateUser();
+            var cuenta = (await UserService.GetUserWithCuentum(usuario.IdUsuario)).Cuentum!;
+            var subasta = await CreateSubasta();
+
+            var puja = new Puja
+            {
+                FechaPuja = DateTime.Now,
+                IdUsuario = usuario.IdUsuario,
+                IdSubasta = subasta.IdSubasta,
+                MontoPuja = 200.00m
+            };
+
+            if (puja.MontoPuja > cuenta.Saldo)
+                Assert.Fail($"Saldo insuficiente del usuario {usuario.CorreoUsuario}");
+
+            var pujas = await PujaService.GetAllByPredicateAsync(p => p.IdSubasta == puja.IdSubasta);
+
+            if (pujas.Any() && puja.MontoPuja <= pujas.Max(p => p.MontoPuja))
+                Assert.Fail($"La puja actual: {pujas.Max(p => p.MontoPuja)} es mayor al monto enviado: {puja.MontoPuja}");
+
+            cuenta.Saldo -= puja.MontoPuja;
+            Assert.True(await CuentaService.UpdateCuenta(cuenta), "No se actualizó el saldo");
+
+            var nuevaPuja = await PujaService.CreateAsync(puja);
+            Assert.NotNull(nuevaPuja);
+
+            var subastaActualizada = await SubastaService.GetSubastaWithPujaAndUsers(subasta.IdSubasta);
+            var participaciones = subastaActualizada.Pujas
+                .Where(p => p.IdUsuario == usuario.IdUsuario)
+                .ToList();
+
+            Assert.NotEmpty(participaciones);
+        }
+
+        [Fact(DisplayName = "10 - Usuario puede recargar")]
+        public async Task RechargeBalance_WithValidAmount_ShouldIncreaseUserBalance()
+        {
+            var usuario = await CreateUser();
+            var cuenta = (await UserService.GetUserWithCuentum(usuario.IdUsuario)).Cuentum!;
+            cuenta.Saldo += 10000;
+            await UserService.UpdateAsync(usuario);
+            Assert.True(cuenta.Saldo > 1000.00M);
+        }
+
+        // ----------------------------
+        // MÉTODOS AUXILIARES PRIVADOS
+        // ----------------------------
+        private async Task<Usuario> CreateUser()
+        {
+            var menu = await MenuService.CreateIfNotExistsAsync(new Menu { EstaActivo = true, NombreMenu = "Authentication" });
             var permiso = await PermisoService.CreateIfNotExistsAsync(new Permiso
             {
                 EstaActivo = true,
@@ -163,15 +168,11 @@ namespace Subastas.Test
                 IdMenuNavigation = menu
             });
 
-            var adminRole = await RolService.CreateIfNotExistsAsync(new Role
+            var role = await RolService.CreateIfNotExistsAsync(new Role
             {
                 EstaActivo = true,
                 NombreRol = "Admin",
-                RolPermisos = [new RolPermiso
-                {
-                    IdPermiso = permiso.IdPermiso,
-                    EstaActivo = true
-                }]
+                RolPermisos = [new RolPermiso { EstaActivo = true, IdPermiso = permiso.IdPermiso }]
             });
 
             return await UserService.CreateIfNotExistsAsync(new Usuario
@@ -179,50 +180,54 @@ namespace Subastas.Test
                 CorreoUsuario = "ia.alexismartinez@ufg.edu.sv",
                 NombreUsuario = "Alexis",
                 ApellidoUsuario = "Martínez",
-                Cuentum = new Cuenta
-                {
-                    Saldo = 1000.00M,
-                    EstaActivo = true
-                },
                 EstaActivo = true,
                 Password = EncrypManager.Encrypt("MiContraseñaEnEntornoDev"),
-                UsuarioRols = new Collection<UsuarioRol>
-                {
-                    new UsuarioRol
-                    {
-                        EstaActivo = true,
-                        IdRol = adminRole.IdRol
-                    }
-                }
+                Cuentum = new Cuenta { EstaActivo = true, Saldo = 1000.00M },
+                UsuarioRols = [new UsuarioRol { EstaActivo = true, IdRol = role.IdRol }]
             });
         }
 
-        public async Task<Producto> CreateProducto()
+        private async Task<Producto> CreateProducto()
         {
-            var producto = new Producto
+            return await ProductoService.CreateAsync(new Producto
             {
                 NombreProducto = "Producto de Unit Testing",
-                DescripcionProducto = "Producto utilizado en pruebas de unit testing para asociarlo a una subasta.",
-                ImagenProducto = "dirección de una imagen ficticia"
-            };
-
-            return await ProductoService.CreateAsync(producto);
+                DescripcionProducto = "Producto utilizado en pruebas de unit testing.",
+                ImagenProducto = "ruta/ficticia.jpg"
+            });
         }
 
-        public async Task<Domain.Subasta> CreateSubasta()
+        private async Task<Domain.Subasta> CreateSubasta()
         {
             var producto = await CreateProducto();
-
-            var subasta = new Domain.Subasta
+            return await SubastaService.CreateAsync(new Domain.Subasta
             {
-                TituloSubasta = "Subasta en Unit Testing",
-                MontoInicial = 100.00m,
+                IdProducto = producto.IdProducto,
                 FechaSubasta = DateTime.Now,
                 FechaSubastaFin = DateTime.Now.AddDays(1),
-                IdProducto = producto.IdProducto
+                EstaActivo = true,
+                TituloSubasta = "Subasta en Unit Testing"
+            });
+        }
+
+        private async Task<Puja> CreatePuja()
+        {
+            var usuario = await CreateUser();
+            var cuenta = (await UserService.GetUserWithCuentum(usuario.IdUsuario)).Cuentum!;
+            var subasta = await CreateSubasta();
+
+            var puja = new Puja
+            {
+                FechaPuja = DateTime.Now,
+                IdUsuario = usuario.IdUsuario,
+                IdSubasta = subasta.IdSubasta,
+                MontoPuja = 200.00m
             };
 
-            return await SubastaService.CreateAsync(subasta);
+            cuenta.Saldo -= puja.MontoPuja;
+            await CuentaService.UpdateCuenta(cuenta);
+
+            return await PujaService.CreateAsync(puja);
         }
     }
 }
